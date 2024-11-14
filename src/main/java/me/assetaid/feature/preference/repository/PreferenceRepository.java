21c317d9842665c987f98d45569aeec4b0fc2eea
psm1st <145013061+psm1st@users.noreply.github.com>
2024-11-15 01:31:13 +0900

    fix : deposit ì recommend API료

--- src/main/java/me/assetaid/feature/preference/repository/PreferenceRepository.java
+++ src/main/java/me/assetaid/feature/preference/repository/PreferenceRepository.java
@@ -3,11 +3,13 @@ package me.assetaid.feature.preference.repository;
 import me.assetaid.feature.preference.repository.entity.PreferenceEntity;
 import me.assetaid.element.user.repository.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PreferenceRepository extends JpaRepository<PreferenceEntity,Long> {
     Optional<PreferenceEntity> findByUser(UserEntity user);
+    @Query("SELECT p FROM PreferenceEntity p JOIN p.user u WHERE u.userId = :userId")
     Optional<PreferenceEntity> findByUserId(String userId);
 }
